5a93cd302f2cd3103ebfab12b523cba74acae7d2
zhulinpinyu <mulixiang199016@gmail.com>
2015-06-10 00:58:56 +0800

    refactor code

--- javascript/map.js
+++ javascript/map.js
@@ -2,8 +2,8 @@ var map_box_layer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y
   maxZoom: 18,
   id: 'examples.map-i875mjb7'
 });
-
-var my_wms = L.tileLayer.wms("http://10.211.55.21:8080/geoserver/test/wms", {
+var HOST_IP = "172.15.1.195"
+var my_wms = L.tileLayer.wms("http://"+HOST_IP+":8080/geoserver/test/wms", {
     layers: 'test:stores_geoosm',
     format: 'image/png',
     transparent: true,
@@ -22,7 +22,7 @@ function Identify (e) {
   var HEIGHT = map.getSize().y;
   var X = map.layerPointToContainerPoint(e.layerPoint).x;
   var Y = map.layerPointToContainerPoint(e.layerPoint).y;
-  var URL = "http://10.211.55.21:8080/geoserver/test/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&FORMAT=image%2Fpng&TRANSPARENT=true&QUERY_LAYERS=test%3Astores_geoosm&LAYERS=test%3Astores_geoosm&STYLES&INFO_FORMAT=application/json&FEATURE_COUNT=1&X="+X+"&Y="+Y+"&SRS=EPSG%3A4326&WIDTH="+WIDTH+"&HEIGHT="+HEIGHT+"&BBOX="+BBOX
+  var URL = "http://"+HOST_IP+":8080/geoserver/test/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&FORMAT=image%2Fpng&TRANSPARENT=true&QUERY_LAYERS=test%3Astores_geoosm&LAYERS=test%3Astores_geoosm&STYLES&INFO_FORMAT=application/json&FEATURE_COUNT=1&X="+X+"&Y="+Y+"&SRS=EPSG%3A4326&WIDTH="+WIDTH+"&HEIGHT="+HEIGHT+"&BBOX="+BBOX
 
   $.ajax({
     url:URL,
