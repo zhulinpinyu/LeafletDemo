d4f383e6aa29ebdff9adc058892cff38c35b2aa6
zhulinpinyu <mulixiang199016@gmail.com>
2015-06-10 16:11:47 +0800

    Refactor folder

---
+++ heatmap_demo/javascript/map.js
@@ -0,0 +1,98 @@
+var HOST_IP = "10.211.55.21";
+var mapbox_layer;
+var wms_layer;
+var heatmap_layer;
+var map;
+
+
+mapbox_layer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png',{
+  maxZoom: 18,
+  id: 'examples.map-i875mjb7'
+});
+
+wms_layer = L.tileLayer.wms("http://"+HOST_IP+":8080/geoserver/test/wms", {
+  layers: 'test:stores_geoosm',
+  format: 'image/png',
+  transparent: true,
+  pointerCursor: true
+});
+
+
+map = L.map('map',{
+  layers:[mapbox_layer]
+}).setView([22.53,114.03],12);
+
+//Init Heat_map
+//setup_heat_layer();
+
+map.on('click', Identify);
+//map.on('zoomend', setup_heat_layer);
+
+function Identify (e) {
+  var BBOX = map.getBounds().toBBoxString();
+  var WIDTH = map.getSize().x;
+  var HEIGHT = map.getSize().y;
+  var X = map.layerPointToContainerPoint(e.layerPoint).x;
+  var Y = map.layerPointToContainerPoint(e.layerPoint).y;
+  var URL = "http://"+HOST_IP+":8080/geoserver/test/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&FORMAT=image%2Fpng&TRANSPARENT=true&QUERY_LAYERS=test%3Astores_geoosm&LAYERS=test%3Astores_geoosm&STYLES&INFO_FORMAT=application/json&FEATURE_COUNT=1&X="+X+"&Y="+Y+"&SRS=EPSG%3A4326&WIDTH="+WIDTH+"&HEIGHT="+HEIGHT+"&BBOX="+BBOX
+
+  $.ajax({
+    url:URL,
+    datatype: "html",
+    type: "GET",
+    contentType: 'application/json; charset=UTF-8',
+    success: function(data) {
+      store = data.features[0]
+      if(store != undefined){
+        popup = L.popup()
+                 .setLatLng(e.latlng)
+                 .setContent("<table>"+
+                             "<tr><td>BizName:</td><td>"+store.properties.lgl_biz_name+"</td></tr>"+
+                             "<tr><td>StoreName:</td><td>"+store.properties.lgl_name_local+"</td></tr>"+
+                             "<tr><td>Address:</td><td>"+store.properties.lgl_raw_address+"</td></tr>"+
+                             "</table>");
+        map.openPopup(popup);
+      }
+      //alert(JSON.stringify(data));
+    }
+ });
+}
+
+function get_url() {
+  var BBOX = map.getBounds().toBBoxString();
+  var WIDTH = map.getSize().x;
+  var HEIGHT = map.getSize().y;
+  return "http://"+HOST_IP+":8080/geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test:stores_geoosm&minFeatures=0&outputFormat=application%2Fjson&SRS=EPSG%3A4326&WIDTH="+WIDTH+"&HEIGHT="+HEIGHT+"&BBOX="+BBOX
+}
+
+function load_heat(data) {
+  var heat_data = []
+  $.each(data.features, function(key, val){
+    var lat = val.geometry.coordinates[1];
+    var lng = val.geometry.coordinates[0];
+    var latLng = L.latLng(lat,lng);
+    heat_data.push(latLng);
+  });
+  console.log(heat_data.length);
+  heatmap_layer = L.heatLayer(heat_data, {radius: 18, blur: 30}).addTo(map);
+}
+
+function setup_map_control(){
+  baseMaps = {
+    "Mapbox": mapbox_layer
+  };
+  overlays = {
+    "HeatMap": heatmap_layer,
+    "WMS": wms_layer
+  };
+  map.addControl(new L.control.layers(baseMaps, overlays, {collapsed: true}));
+}
+
+$.ajax({
+  url:get_url(),
+  datatype: "html",
+  type: "GET",
+  contentType: 'application/json; charset=UTF-8',
+  success: load_heat,
+  complete: setup_map_control
+ });
